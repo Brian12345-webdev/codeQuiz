ddb298292ba72d3c6b76cb26f347a6860df3bc90
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-13 20:18:48 -0500

    stating code for js, starting at line 33 may need to be changed

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,47 @@
 // do a function with a confirm prompt to ask the user if they want to start coding quiz
-var startQuiz = document.querySelector("startQuiz");
+const startButton = document.getElementById('start-btn');
+const previousButton = document.getElementById('previous-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainer = document.getElementById('question-container');
+const timerEl = document.getElementById('timer');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('btn-answers');
 
+let shuffleQuestions, currentQuestionIndex;
+startButton.addEventListener('click', startQuiz);
+previousButton.addEventListener('click', () => {
+  currentQuestionIndex--
+  setNextQuestion()
+});
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++ 
+  setNextQuestion()
+});
 
 function startQuiz() {
-var yourCodeQuiz = confirm("Do you want to start your coding quiz?");
-if (yourCodeQuiz === true) {
-// need fxn to show first question and so on();
-} else {
-alert("Come Back Soon!");
+  startButton.classList.add('hide');
+  shuffleQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  setNextQuestion();
 }
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffleQuestions[currentQuestionIndex]);
 }
+
+function showQuestion(question) {
+  questionElement.innerText = question.title;
+  question.alternatives.forEach((choice, index) => {
+    const button = document.createElement('button');
+    button.innerText = choice;
+    button.classList.add('btn-answers');
+    button.setAttribute('data-index', index);
+    button.addEventListener('click', checkAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
 // button function if (confirm) button pressed start coding quiz.
 // Add ""element".addEventListner("click", function);  to each question.
 // answers that are right or wrong must be counted and a collected percentage total will be displayed.
